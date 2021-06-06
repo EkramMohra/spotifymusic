fa2afd487ce795962f78b0ec50d32adb013a17f7
Afeef <afeef.as11@gmail.com>
2021-06-06 13:56:10 +0300

    added meaningfull names

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,6 @@
 const render = new Renderer()
 
+//extract access_token from url
 const getUrlParameter = (sParam) => {
     let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
         sURLVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [], sParameterName, i;
@@ -15,19 +16,16 @@ const getUrlParameter = (sParam) => {
 const accessToken = getUrlParameter('access_token');
 
 let client_id = '30264da956bb4d909269af1e165230a1';
-    // Use the following site to convert your regular url to the encoded version:
-    // https://www.url-encode-decode.com/
-    let redirect_uri = 'https://spotify-musicplayer.herokuapp.com';
-    // *************** END *************************
+let redirect_uri = 'https://spotify-musicplayer.herokuapp.com';
 
-    const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
-    // Don't authorize if we have an access token already
-    if(accessToken == null || accessToken == "" || accessToken == undefined){
-      window.location.replace(redirect);
-    }
-//logic
+const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
 
-let arr=[];
+// Don't authorize if we have an access token already
+if(accessToken == null || accessToken == "" || accessToken == undefined){
+    window.location.replace(redirect);
+}
+
+
 $('.search-container').on('click', '#search', function() {
     let val = $('.input').val()
     let search_query = encodeURI(val);
@@ -36,20 +34,16 @@ $('.search-container').on('click', '#search', function() {
           // Load our songs from Spotify into our page
           let num_of_tracks = data.tracks.items.length;
           let count = 0;
-          // Max number of songs is 12
           const max_songs = 12;
+          let Sources = [];
+          
           while(count < max_songs && count < num_of_tracks){
-            // Extract the id of the FIRST song from the data object
             let id = data.tracks.items[count].id;
-            // Constructing two different iframes to embed the song
             let src_str = `https://open.spotify.com/embed/track/${id}`;
-            arr.push(src_str);
-            // let iframe = `<div class='song'><iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
-
-            // $('.container').append(iframe);
+            Sources.push(src_str);
             count++;
           }
 
-          render.render(arr)
+          render.render(Sources)
     })
 })
